cd0dde9ffd416f41e1b6165276b524ac681af5a9
IlijazM <mehmedovic.ilijaz@gmail.com>
2022-12-11 07:27:50 +0100

    add simple frontend layout

---
+++ icon-designer/frontend/src/app/routes/icons/(icon).component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  templateUrl: '(icon).component.html',
+  styleUrls: ['(icon).component.css'],
+})
+export default class AboutPageComponent {}
